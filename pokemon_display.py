d482dc9c09ab5a906b1d93a6593def42ac4cd7ec
Sinead Dart <32309104+Sinead96@users.noreply.github.com>
2025-05-19 11:01:01 +0100

    Create pokemon_display.py

---
+++ pokemon_display.py
@@ -0,0 +1,32 @@
+class Pokemon:
+  def __init__(self, entry, name, types, description, is_caught):
+    self.entry = entry 
+    self.name = name 
+    self.types = types
+    self.description = description 
+    self.is_caught = is_caught
+
+  def speak (self): 
+    print(name + name)
+
+  def display_details (self):
+    print (f'Entry Name: {self.entry}')
+    print (f'Name: {self.name}')
+    print (f'Type: {self.types}')
+    print (f'Description: {self.description}')
+    if self.is_caught == True: 
+      print (f'{self.name} has already been caught!')
+    elif self.is_caught == False: 
+      print (f'{self.name} has not been caught yet!')
+
+clefairy = Pokemon (35, 'Clefairy', 'Fairy', 'On nights with a full moon, Clefairy gather from all over and dance. Bathing in moonlight makes them float.', True)
+
+clefairy.display_details()
+
+ninetales = Pokemon (38, 'Ninetales', 'Fire', 'Some legends claim that each of its nine tails has its own unique type of special mystical power.', False)
+
+ninetales.display_details()
+
+ditto = Pokemon (132, 'Ditto', 'Normal', 'Its transformation ability is perfect. However, if made to laugh, it can’t maintain its disguise.', False)
+
+ditto.display_details()
